7f0c83c27e89f69dc0c4e1cb50d2ff1365e6690b
미치삶 <michisarm@gmail.com>
2020-09-16 07:10:07 +0900

    chart

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,115 +1,115 @@
 export const getSamplePanelData = () => {
   const data = [
   {
-    date: "1994",
+    category: "1994",
     x: 1587,
     y: 650,
     z: 121
   },
   {
-    date: "1995",
+    category: "1995",
     x: 1567,
     y: 683,
     z: 146
   },
   {
-    date: "1996",
+    category: "1996",
     x: 1617,
     y: 691,
     z: 138
   },
   {
-    date: "1997",
+    category: "1997",
     x: 1630,
     y: 642,
     z: 127
   },
   {
-    date: "1998",
+    category: "1998",
     x: 1660,
     y: 699,
     z: 105
   },
   {
-    date: "1999",
+    category: "1999",
     x: 1683,
     y: 721,
     z: 109
   },
   {
-    date: "2000",
+    category: "2000",
     x: 1691,
     y: 737,
     z: 112
   },
   {
-    date: "2001",
+    category: "2001",
     x: 1298,
     y: 680,
     z: 101
   },
   {
-    date: "2002",
+    category: "2002",
     x: 1275,
     y: 664,
     z: 97
   },
   {
-    date: "2003",
+    category: "2003",
     x: 1246,
     y: 648,
     z: 93
   },
   {
-    date: "2004",
+    category: "2004",
     x: 1218,
     y: 637,
     z: 101
   },
   {
-    date: "2005",
+    category: "2005",
     x: 1213,
     y: 633,
     z: 87
   },
   {
-    date: "2006",
+    category: "2006",
     x: 1199,
     y: 621,
     z: 79
   },
   {
-    date: "2007",
+    category: "2007",
     x: 1110,
     y: 210,
     z: 81
   },
   {
-    date: "2008",
+    category: "2008",
     x: 1165,
     y: 232,
     z: 75
   },
   {
-    date: "2009",
+    category: "2009",
     x: 1145,
     y: 219,
     z: 88
   },
   {
-    date: "2010",
+    category: "2010",
     x: 1163,
     y: 201,
     z: 82
   },
   {
-    date: "2011",
+    category: "2011",
     x: 1180,
     y: 285,
     z: 87
   },
   {
-    date: "2012",
+    category: "2012",
     x: 1159,
     y: 277,
     z: 71
